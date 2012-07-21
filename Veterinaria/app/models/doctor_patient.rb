b4946a213682b4d36abccfee312c3b562a333bf3
mgonzales <mgonzalesh@gmail.com>
2012-07-20 20:16:50 -0500

    relacion doctor paciente

--- Veterinaria/app/models/doctor_patient.rb
+++ Veterinaria/app/models/doctor_patient.rb
@@ -1,2 +1,4 @@
 class DoctorPatient < ActiveRecord::Base
+	belongs_to: doctor
+	belongs_to: patient
 end
--- Veterinaria/app/models/patient.rb
+++ Veterinaria/app/models/patient.rb
@@ -1,4 +1,6 @@
 class Patient < ActiveRecord::Base
  has_many :owner_patients
  has_many :owners, :through => :owner_patients
+ has_many :doctor_patients
+ has_many :doctors, :through => :doctors_patients
 end
