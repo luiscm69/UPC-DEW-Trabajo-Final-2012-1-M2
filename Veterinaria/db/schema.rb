1e583d6569b7080d77fa208ca7b7d41253a8b26e
MarcosLear <marcos_808@hotmail.com>
2012-07-24 20:49:06 -0500

    Tabla article

--- Veterinaria/db/schema.rb
+++ Veterinaria/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120725011752) do
+ActiveRecord::Schema.define(:version => 20120725014819) do
+
+  create_table "articles", :force => true do |t|
+    t.string   "articlename"
+    t.decimal  "price"
+    t.integer  "stock"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "doctor_patients", :force => true do |t|
     t.integer  "doctor_id"
